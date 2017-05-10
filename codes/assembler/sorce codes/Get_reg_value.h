1f79ae99bd83cb613a1b37e4e9fe8958b00cf6b1
Jinglei_Cheng_THU <chengjl14@mails.tsinghua.edu.cn>
2017-05-10 10:23:27 +0800

    deleted

--- codes/assembler/sorce codes/Get_reg_value.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef GET_REG_VALUE_H_
-#define GET_REG_VALUE_H_ "Get_reg_value"
-
-string Get_reg_value(string reg);  //根据寄存器名str获取寄存器号
-
-#endif
--- codes/assembler/sorce codes/assembler.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef ASSEMBLER_H_
-#define ASSEMBLER_H_  "assembler.h"
-
-void assembler(string ir_set[],string label_value[],int label_locate[],int label_num);
-
-#endif
